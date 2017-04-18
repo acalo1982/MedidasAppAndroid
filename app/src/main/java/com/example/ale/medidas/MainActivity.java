8a8dad7777b12e8fba1a276258a89e8fd32f6dd3
acalo1982 <ale.arroba@gmail.com>
2017-04-18 15:31:30 +0200

    Initial commit

---
+++ app/src/main/java/com/example/ale/medidas/MainActivity.java
@@ -0,0 +1,46 @@
+package com.example.ale.medidas;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.content.Intent;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    //Asociamos el menu a la ActionBar por defecto de la App
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    //Detecta qué botón de la ActionBar se ha pulsado
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_nuevo:
+                Log.i("ActionBar", "Nuevo!");
+                return true;
+            case R.id.action_buscar: //Pulsado sobre el icono de configuracion
+                Log.i("ActionBar", "Settings!");
+                //Intent intent = new Intent(this, Configuracion.class); // Abrimos la pantalla de configuracion: Implementada como una Activity
+                Intent intent = new Intent(this, OpcionesActivity.class); // Implementada como una PreferenceScreen
+                startActivity(intent);
+                return true;
+            case R.id.action_settings:
+                Log.i("ActionBar", "Settings!");;
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+}
