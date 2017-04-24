c9f60b338a561babf1025977f6a5bde222656f33
acalo1982 <ale.arroba@gmail.com>
2017-04-24 13:18:39 +0200

    Captura el evento Click sobre los ejes de la Actividad principal Se ha añadido también una clase que implmenta la comunicación TCP/IP

--- app/src/main/java/com/example/ale/medidas/MainActivity.java
+++ app/src/main/java/com/example/ale/medidas/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.ale.medidas;
 
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -9,14 +10,19 @@ import android.content.Intent;
 import com.jjoe64.graphview.GraphView;
 import com.jjoe64.graphview.series.*;
 import android.content.pm.ActivityInfo;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TCPClient mTcpClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         super.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
 
+        // OPCIONES RELACIONADAS CON LA ACTIONBAR
         setContentView(R.layout.activity_main);
 //        getSupportActionBar().setDisplayShowTitleEnabled(true);
 //        getSupportActionBar().setTitle("Ale");
@@ -26,39 +32,47 @@ public class MainActivity extends AppCompatActivity {
 //        //getSupportActionBar().setLogo(R.drawable.dani_icon);
         //getSupportActionBar().setDisplayUseLogoEnabled(true);
 
+        //CREAMOS UNOS EJES Y UNA CURVA DE EJEMPLO
         GraphView graph = (GraphView) findViewById(R.id.graph);
-        DataPoint[] points = new DataPoint[100];
-        for (int i = 0; i < points.length; i++) {
+        DataPoint[] points = new DataPoint[100]; // array de objetos DataPoint
+        for (int i = 0; i < points.length; i++) { //inicializamos el array de DataPoints con valores (x,y)
             points[i] = new DataPoint(i, Math.sin(i*0.5) * 20*(Math.random()*10+1));
         }
-        LineGraphSeries<DataPoint> series = new LineGraphSeries<>(points);
-        // set manual X bounds
+        LineGraphSeries<DataPoint> series = new LineGraphSeries<>(points); //Creamos una curva a partir del array de puntos
+        // set manual Y bounds
         graph.getViewport().setYAxisBoundsManual(true);
         graph.getViewport().setMinY(-150);
         graph.getViewport().setMaxY(150);
-
+        // set manual X bounds
         graph.getViewport().setXAxisBoundsManual(true);
         graph.getViewport().setMinX(4);
         graph.getViewport().setMaxX(80);
-
         // enable scaling and scrolling
         graph.getViewport().setScalable(true);
         graph.getViewport().setScalableY(true);
+        graph.addSeries(series); //Dibuamos la curva sobre los ejes
 
-        graph.addSeries(series);
+        //EVENTO DE PULSAR SOBRE EL EJE (LO TRATAREMOS COMO UN OBJETO VIEW)
+        graph.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getApplicationContext(), "Click sobre los ejes!", Toast.LENGTH_SHORT).show();
 
-        //        LineGraphSeries<DataPoint> series = new LineGraphSeries<>(new DataPoint[] {
-//                new DataPoint(0, 1),
-//                new DataPoint(1, 5),
-//                new DataPoint(2, 3)
-//        });
-//        graph.addSeries(series);
+                //sends the message to the server
+//                if (mTcpClient != null) {
+//                    String message="comandoTCP\n";//comando indicando al VNA que realice una captura!
+//                    mTcpClient.sendMessage(message);
+//                }
 
+            }
+        });
+
+        //CREAMOS UN OBEJETO QUE ATENDERÁ LOS MSG ENVIADOS POR EL VNA: LOS DATOS DE LA LECTURA
+        //new connectTask().execute("");
     }
 
 
     //Asociamos el menu a la ActionBar por defecto de la App
-    //ale
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
@@ -86,4 +100,34 @@ public class MainActivity extends AppCompatActivity {
                 return super.onOptionsItemSelected(item);
         }
     }
+
+    public class connectTask extends AsyncTask<String,String,TCPClient> {
+
+        @Override
+        protected TCPClient doInBackground(String... message) {
+
+            //we create a TCPClient object and
+            mTcpClient = new TCPClient(new TCPClient.OnMessageReceived() {
+                @Override
+                //here the messageReceived method is implemented
+                public void messageReceived(String message) {
+                    //this method calls the onProgressUpdate
+                    publishProgress(message);
+                }
+            });
+            mTcpClient.run(); //abrimos el socket y empezamos a escuchar datos del servidor
+
+            return null;
+        }
+
+        @Override
+        protected void onProgressUpdate(String... values) {
+            super.onProgressUpdate(values);
+
+
+        }
+    }
+
 }
+
+
