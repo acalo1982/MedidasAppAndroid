32fa1cef010a4356ce9c15715ad85fee9d99a69e
acalo1982 <ale.arroba@gmail.com>
2017-04-26 15:44:07 +0200

    Lectura del S11 de la pantalla y dibujarlo en una gráfica

--- app/src/main/java/com/example/ale/medidas/MainActivity.java
+++ app/src/main/java/com/example/ale/medidas/MainActivity.java
@@ -20,8 +20,10 @@ public class MainActivity extends AppCompatActivity {
 
     private TCPClient mTcpClient; //objeto que recivirá y enviará msg al servidor!
     private TCPClientv2 mTcpClientv2;
-    private TCPClientv0 mTcpClientv0;
-    private int conectado=0;//
+    private int conectado=0;//monitoriza el estado de conexión al VNA
+    private String S11=null;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -58,13 +60,6 @@ public class MainActivity extends AppCompatActivity {
         graph.getViewport().setScalableY(true);
         graph.addSeries(series); //Dibuamos la curva sobre los ejes
 
-        //CREAMOS UN SOCKET DE COMUNICACIÓN
-        //Cliente v2: compartimos un mismo socket para enviar varios comandos!
-        //mTcpClientv2 = new TCPClientv2(); //creamos objeto y asiganamos una referencia a él
-        //new connectTaskv2().execute(); //abrimos socket (v2)
-
-        //Cliente v0 (NO FUNCIONA)
-        //new connectTaskv0().execute(); //abrimos socket (v0)
 
         //EVENTO: PULSAR SOBRE EL EJE (LO TRATAREMOS COMO UN OBJETO VIEW)
         graph.setOnClickListener(new View.OnClickListener() {
@@ -76,27 +71,36 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void lecturaS11(View v){
-        //LECTURA
-        //new connectTask().execute(":DISP:WIND:TRAC1:Y:AUTO");
+        //LECTURA de los datos del VNA
         new connectTask().execute(":CALC:DATA:SDAT?");
 
-        //Cliente v0: Codigo Original probado con el servidor del VNA
-//                Log.e("MainActivity", "alej: Antes de llamar a mTcpClient.sendMessage()");
-//                if (mTcpClientv0 != null) {
-//                    mTcpClientv0.sendMessage(":DISP:WIND:TRAC1:Y:AUTO");
-//                    Log.e("MainActivity", "alej: Despues de llamar a mTcpClient.sendMessage()");
-//                }
+        //Convertimos el String en DataPoint
+        String[] S11_list=S11.split(",");
+        int N=S11_list.length/2;//string array con la parte real y compleja del S11 para cada freq
+        int df=(18-2)/(N-1);//incremento en freq
+        DataPoint[] points = new DataPoint[N];
+        for (int i = 0; i < N; i+=2) {
+            double yR=Float.parseFloat(S11_list[i]);
+            double yI=Float.parseFloat(S11_list[i+1]);
+            double M=Math.sqrt(Math.pow(yR,2)+Math.pow(yI,2));//modulo
+            points[i] = new DataPoint(2+i*df,M);
+        }
 
-        //Cliente v1: Se abre y cierra un socket para comando
-        //new connectTask().execute(":CALC:DATA:SDAT?");//nuevo hilo: se abre un socket, se envía 1 comando y se espera su respuesta
-
-
-        //Cliente v2: Se abre un socket y se deja abierto para enviar todos los comandos
-        //new connectTaskv3().execute();//leemos datos del servidor (v2)
-
-
-
-
+        //Dibujamos la curva leida
+        GraphView graph = (GraphView) findViewById(R.id.graph);
+        LineGraphSeries<DataPoint> series = new LineGraphSeries<>(points); //Creamos una curva a partir del array de puntos
+        // set manual Y bounds
+        graph.getViewport().setYAxisBoundsManual(true);
+        graph.getViewport().setMinY(-35);
+        graph.getViewport().setMaxY(5);
+        // set manual X bounds
+        graph.getViewport().setXAxisBoundsManual(true);
+        graph.getViewport().setMinX(2);
+        graph.getViewport().setMaxX(18);
+        // enable scaling and scrolling
+        graph.getViewport().setScalable(true);
+        graph.getViewport().setScalableY(true);
+        graph.addSeries(series); //Dibuamos la curva sobre los ejes
     }
 
     //Asociamos el menu a la ActionBar por defecto de la App
@@ -130,14 +134,6 @@ public class MainActivity extends AppCompatActivity {
                     //Configuración del VNA
                     String[] cmd_conf={":INST \"NA\"",":SOUR:POW:ALC HIGH",":INIT:CONT 1",":FREQ:STAR 2e9",":FREQ:STOP 18e9",":SWE:POIN 201",":BWID 1000",":AVER:COUN 1"};
                     new connectTask().execute(cmd_conf);
-//                    new connectTask().execute(":INST NA");
-//                    new connectTask().execute(":SOUR:POW:ALC HIGH");
-//                    new connectTask().execute(":INIT:CONT 1");
-//                    new connectTask().execute(":FREQ:STAR 2");
-//                    new connectTask().execute(":FREQ:STOP 18");
-//                    new connectTask().execute(":SWE:POIN 201");
-//                    new connectTask().execute(":BWID 1000");
-//                    new connectTask().execute(":AVER:COUN 1");
                 }else{
                     conectado = 0;
                     item.setIcon(android.R.drawable.presence_invisible);
@@ -175,37 +171,11 @@ public class MainActivity extends AppCompatActivity {
         protected void onProgressUpdate(String... values) {
             super.onProgressUpdate(values);
             String msg = values[0]; //recuperamos el texto como parámetro de entrada
-            Toast.makeText(getApplicationContext(), "Rxdo msg=" + msg, Toast.LENGTH_SHORT).show();
+            Toast.makeText(getApplicationContext(), "Num de puntos: " + msg.length() + " Rxdo msg=" + msg, Toast.LENGTH_SHORT).show();
             //mTcpClient.stopClient();
         }
     }
 
-    //Cliente v0: Código original NO FUNCIONA (comparte un mismo socket para enviar todos los comandos y escuchar las respuestas del servidor)
-    public class connectTaskv0 extends AsyncTask<String, String, TCPClient> {
-
-        @Override
-        protected TCPClient doInBackground(String... message) {
-            //we create a TCPClient object and
-            mTcpClientv0 = new TCPClientv0(new TCPClientv0.OnMessageReceived() {
-                @Override
-                //here the messageReceived method is implemented
-                public void messageReceived(String message) {
-                    //this method calls the onProgressUpdate
-                    publishProgress(message);
-                }
-            });
-            mTcpClientv0.run(); //abrimos el socket de comunicacion con el servidor
-            return null;
-        }
-
-        @Override
-        protected void onProgressUpdate(String... values) {
-            super.onProgressUpdate(values);
-            String msg = values[0]; //recuperamos el texto como parámetro de entrada
-            Toast.makeText(getApplicationContext(), "Rxdo msg=" + msg, Toast.LENGTH_SHORT).show();
-            //mTcpClient.stopClient();
-        }
-    }
 
     //Cliente v2: Comparte un socket para enviar varios comandos y escuchar las respuestas enviadas por el servidor
     public class connectTaskv2 extends AsyncTask<Void, String, Boolean> {
@@ -227,8 +197,8 @@ public class MainActivity extends AppCompatActivity {
         protected void onProgressUpdate(String... values) {
             super.onProgressUpdate(values);
             String msg = values[0]; //recuperamos el texto como parámetro de entrada
-            Toast.makeText(getApplicationContext(), "Rxdo msg=" + msg, Toast.LENGTH_SHORT).show();
-            //mTcpClient.stopClient();
+            S11 = values[0];
+            Toast.makeText(getApplicationContext(), "Leído S11=" + S11, Toast.LENGTH_SHORT).show();
         }
     }
 
